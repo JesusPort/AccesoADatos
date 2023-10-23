32ba1e98930a8c9d35760284476e8158b2f3188e
alumno <alumno@SMR2B005>
2023-10-23 07:24:09 +0200

    Raro Json

--- EjConversionJson/src/control/ConvertirJsonJava.java
+++ EjConversionJson/src/control/ConvertirJsonJava.java
@@ -1,6 +1,7 @@
 package control;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.google.gson.Gson;
@@ -11,15 +12,24 @@ import modelo.Alumno;
 public class ConvertirJsonJava {
 
 	static List<Alumno> alumnosList;
+	static String jsonString;
 	public static void main(String[] args) {
 		alumnosList = new ArrayList<Alumno>();
 		inicializarLista();
 		escribirJson();
+		leerJson();
 		
 	}
 	
+	private static void leerJson() {
+		Gson gson= new Gson();
+		Alumno[] arrayAlumnos= gson.fromJson(jsonString, Alumno[].class);
+		List<Alumno> listaAlumnos= Arrays.asList(arrayAlumnos);
+		System.out.println(listaAlumnos);
+	}
+
 	private static void escribirJson(){
-		String jsonString= new Gson().toJson(alumnosList);
+		jsonString= new Gson().toJson(alumnosList);
 		System.out.println(jsonString);
 		
 	}
